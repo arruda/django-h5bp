ac4298c9af68e1ea88ee806ecf063867fa7b62d7
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-05-04 14:21:45 -0300

    testing if all templates have some blocks to override fix #5

--- tests/tests.py
+++ tests/tests.py
@@ -3,16 +3,54 @@ from django.test import TestCase
 from django.template.loader import Template, Context
 
 
-class BootstrapTeamplteTest(TestCase):
-    BASE_TEMPLATE = """
-        {% extends "bootstrap/bootstrap_h5bp.html" %}
-        """
+class TemplateTestCase(TestCase):
+    BASE_TEMPLATE = """ """
 
     def setUp(self):
         self.template_txt = self.BASE_TEMPLATE
 
-    def test_bla(self):
-        context = Context({"my_name": "Adrian"})
+    def test_can_override_django_h5bp_css_block(self):
+        context = Context({})
+        self.template_txt += """
+
+        {% block django-h5bp-css %}<div>NO-BOCK</div>{% endblock %}
+        """
         template = Template(self.template_txt)
         rendered = template.render(context)
-        self.assertInHTML("""<script src="/static/js/main.js">""", rendered)
+        self.assertInHTML(
+            """<div>NO-BOCK</div>""",
+            rendered,
+            msg_prefix="Could not override django-h5bp-css block. "
+        )
+
+    def test_can_override_django_h5bp_js_block(self):
+        context = Context({})
+        self.template_txt += """
+
+        {% block django-h5bp-js %}<div>NO-BOCK</div>{% endblock %}
+        """
+        template = Template(self.template_txt)
+        rendered = template.render(context)
+        self.assertInHTML(
+            """<div>NO-BOCK</div>""",
+            rendered,
+            msg_prefix="Could not override django-h5bp-js block. "
+        )
+
+
+class BootstrapTeamplateTest(TemplateTestCase):
+    BASE_TEMPLATE = """
+        {% extends "bootstrap/bootstrap_h5bp.html" %}
+        """
+
+
+class ClassicTeamplateTest(TemplateTestCase):
+    BASE_TEMPLATE = """
+        {% extends "classic/classic_h5bp.html" %}
+        """
+
+
+class ResponsiveTeamplateTest(TemplateTestCase):
+    BASE_TEMPLATE = """
+        {% extends "responsive/responsive_h5bp.html" %}
+        """
